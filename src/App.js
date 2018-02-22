ffc7b7f34ebeabf6f546dbf0cec12bc26d6cf670
iZumzi <marius.bordas95@gmail.com>
2018-02-22 13:56:26 +0200

    json

--- src/App.js
+++ src/App.js
@@ -12,6 +12,17 @@ import Lupa from './lupa.png';
 import MoviesListRecent from "./components/moviesListRecentComponent";
 
 
+import fontawesome from '@fortawesome/fontawesome'
+// import faUser from '@fortawesome/fontawesome-free-solid/faUser'
+// import faCircle from '@fortawesome/fontawesome-free-regular/faCircle'
+// import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook'
+
+// fontawesome.library.add(faUser)
+// fontawesome.library.add(faCircle)
+// fontawesome.library.add(faFacebook)
+
+
+
 class App extends React.Component {
   render() {
     return ([
@@ -36,6 +47,7 @@ class App extends React.Component {
         </Link>
         <Link to="/login" component={ Login }>
           <div className="div-login-btn">
+            <i class="fab fa-amilia"></i>
             <span className="login-btn-img" data-toggle="tooltip" title="Login"></span>
           </div>
         </Link>
