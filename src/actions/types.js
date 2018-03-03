bb7fb03cb96a416876253a5e6f119249bd44dd13
Diana Agata Leonte <dyaneddd@yahoo.com>
2018-03-03 22:12:29 +0200

    types and reducers

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,9 +1,7 @@
 export const MOVIE_DATA_RECENT = 'MOVIE_DATA_RECENT';
 export const MOVIE_DATA_ALL = 'MOVIE_DATA_ALL';
 export const MOVIE_DATA_PAGED = 'MOVIE_DATA_PAGED';
-<<<<<<< HEAD
 export const MOVIE_DETAILS = 'MOVIE_DETAILS';
-=======
 export const SEARCH_TITLE = 'SEARCH_TITLE';
 export const SEARCH_RUNTIME = 'SEARCH_RUNTIME';
 export const SEARCH_GENRE = 'SEARCH_GENRE';
@@ -15,4 +13,3 @@ export const SEARCH_VOTES = 'SEARCH_VOTES';
 export const SEARCH_ID = 'SEARCH_ID';
 export const SEARCH_TYPE = 'SEARCH_TYPE';
 export const SEARCH_YEAR = 'SEARCH_YEAR';
->>>>>>> master
