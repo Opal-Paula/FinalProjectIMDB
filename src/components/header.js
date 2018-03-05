2b5d43337d6ea735ca73368d15937c360bfaf031
Paula <deadly_dragon13@yahoo.com>
2018-03-05 21:57:58 +0200

    removed unused link

--- src/components/header.js
+++ src/components/header.js
@@ -8,8 +8,7 @@ class Header extends Component {
             <nav className="nav-bar navbar-nav ml-auto lead">
                 <div className="nav-container">
                     <ul>
-                        <li><Link to="/" className="link-button">Home</Link></li>
-                        <li><Link to="/searchpage" className="link-button">Search Movies</Link></li>
+                        <li><Link to="/" className="link-button">Home</Link></li>                       
                         <li><Link to="/movies" className="link-button">Movies</Link></li>
                     </ul>
                 </div>
